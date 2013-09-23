18dd35632e1348414b381204552c9303cab34759
fpckalk <fpckalk@gmail.com>
2013-09-23 14:31:18 +0200

    Framework stands

--- assignments/week3/9-templates/app.js
+++ assignments/week3/9-templates/app.js
@@ -51,24 +51,67 @@ var APP = APP || {};
 
 				'/schedule': function() {
 					console.log('Schedule called');
+					APP.page.schedule();
 				},
 
 				'/ranking': function() {
 					console.log('Ranking called');
+					APP.page.ranking();
 				},
 
 				'/game': function() {
 					console.log('Game called');
+					APP.page.game();
 				},
 
 				'*': function() {
 					console.log('Catch-all called');
+					APP.page.schedule();
 				}
 
 			});
+		},
+
+		change: function () {
+            var route = window.location.hash.slice(2),
+                sections = qwery('section[data-route]'),
+                section = qwery('[data-route=' + route + ']')[0];  
+
+            // Show active section, hide all other
+            if (section) {
+            	for (var i=0; i < sections.length; i++){
+            		sections[i].classList.remove('active');
+            	}
+            	section.classList.add('active');
+            }
+
+            // Default route
+            if (!route) {
+            	sections[0].classList.add('active');
+            }
+
 		}
 
 	};
+
+
+	// Page
+	APP.page = {
+		schedule: function () {
+			Transparency.render(qwery('[data-route=schedule]')[0], APP.schedule);
+			APP.router.change();
+		},
+
+		ranking: function () {
+			Transparency.render(qwery('[data-route=ranking]')[0], APP.ranking);
+			APP.router.change();
+		},
+
+		game: function () {
+			Transparency.render(qwery('[data-route=game]')[0], APP.game);
+			APP.router.change();
+		}
+	}
 
 	////////////////////// This is a well commented line
 	domready(function() {
