95bd6d0413192df600c4bb5970d29832ca76ddf0
fpckalk <fpckalk@gmail.com>
2013-10-17 16:56:18 +0200

    Retrieved local team data

---
+++ assignments/week4/11.3-leaguevine-api/js/app.js
@@ -0,0 +1,212 @@
+/*
+
+A basic framework skeleton
+
+Leaguevine access token: 82996312dc
+Tournament ID: 19389
+
+Using own made XHR with callbacks
+
+*/
+
+'use strict';
+
+var APP = APP || {};
+
+(function() {
+
+	/* Initiate all the functions you want to start when
+	 * everything has finished loading.
+	*/
+	APP.controller = {
+		init: function() {
+			APP.router.init();
+		}
+
+	};
+
+
+	APP.request = {
+
+		// Request any xml type file by passing up the method and file you're looking for
+		xmlRequest: function(method, file, success) {
+
+			var request = new XMLHttpRequest;
+
+			request.open(method, file);
+			request.send(null);
+
+			request.onreadystatechange = function() {
+				if (request.readyState == 4) {
+					if (request.status == 200) {
+						success(request);
+					}
+				}
+			}
+
+		}
+
+	};
+
+
+	// Apply data to the different 'pages'
+
+	APP.schedule = {
+
+		title: 'Pool A - Schedule',
+		description: 'This is the schedule.'
+
+	};
+
+
+	APP.game = {
+
+		title: 'Game',
+		description: 'This is the game.'
+
+	};
+
+
+	APP.ranking = {
+
+		title: 'Pool A - Ranking',
+		description: 'This is the ranking.'
+
+	};
+
+
+	APP.movies = {
+
+		title: 'Movies',
+		description: 'A collection of movies'
+
+	};
+
+	// APP.movieDirectives = {
+	// 	cover: {
+	// 		src: return this.cover;
+	// 	}
+	// }
+
+
+	// ROUTIE
+	APP.router = {
+
+		/* The functionality of Routie lies in that it can differentiate links
+		 * and add functions to the links called.
+		 * Thus you can 'switch' pages without using another request.
+		*/
+		init: function() {
+			routie({
+
+				'/schedule': function() {
+					APP.page.schedule();
+				},
+
+				'/ranking': function() {
+					APP.page.ranking();
+				},
+
+				'/game': function() {
+					APP.page.game();
+				},
+
+				'/movies': function() {
+					APP.page.movies();
+				},
+
+				'*': function() {
+					APP.page.schedule();
+				}
+
+			});
+		},
+
+		change: function () {
+
+            var route = window.location.hash.slice(2),
+            	articles = qwery('article[data-route]'),
+            	article = qwery('[data-route=' + route + ']')[0];  
+
+            // Show active article, hide all other
+            if (article) {
+            	for (var i=0; i < articles.length; i++){
+            		articles[i].classList.remove('active');
+            	}
+            	article.classList.add('active');
+			}
+
+			// Default route
+			if (!route) {
+            	articles[0].classList.add('active');
+			}
+
+		}
+
+	};
+
+
+	// Page
+	APP.page = {
+
+		schedule: function() {
+			APP.request.xmlRequest('GET', 'data/teams.json', function(data) {
+				data = JSON.parse(data.response);
+
+				Transparency.render(qwery('[data-bind=scheduleData]')[0], data.schedule, APP.directives.schedule(data.schedule));
+				APP.router.change();				
+			})
+		},
+
+		game: function() {
+			Transparency.render(qwery('[data-route=game]')[0], APP.game);
+			APP.router.change();
+		},
+
+		ranking: function() {
+			Transparency.render(qwery('[data-route=ranking]')[0], APP.ranking);
+			APP.router.change();
+		},
+
+		movies: function() {
+			APP.request.xmlRequest('GET', 'data/movies.json', function(data) {
+				data = JSON.parse(data.response);
+
+				Transparency.render(qwery('[data-route=movies]')[0], data, APP.directives.movies(data));
+				APP.router.change();
+			});
+		}
+
+	};
+
+	// All the thanks go to Joost Faber
+	// https://github.com/joostf
+	APP.directives = {
+		movies: function (data) {
+			console.log(data);
+			return {
+				cover: {
+					src: function() {
+						return this.cover;
+					}
+				}
+			}
+		},
+		schedule: function(data) {
+			return {
+				result: {
+					text: function() {
+						return this.team1Score + " - " + this.team2Score;
+					}
+				}
+			}
+		}
+	}
+
+
+	////////////////////// This is a well commented line
+	domready(function() {
+		APP.controller.init();
+	});
+
+})();
